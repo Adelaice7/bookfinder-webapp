0bf4e4fd903f4980f45ef67129c17702aa299c86
Adelaice7 <gotthardramona@gmail.com>
2019-05-14 14:44:21 +0200

    Init

---
+++ target/bookfinderwebapp/WEB-INF/classes/com/bookfinderapi/controller/MainController.java
@@ -0,0 +1,13 @@
+public class MainController extends HttpServlet {
+    
+    public void doServiceGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        
+        response.setContentType("text/html");
+        
+        PrintWriter out = response.getWriter();
+        
+        out.println("<html><body>");
+        out.println("<h1>Book Finder Servlet</h1>");
+        out.println("</body></html>");
+    }
+}
